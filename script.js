e6d1aa22e2b83bd14bc0389610a1890bf2879a6a
Alice Russell <alice.louise.russell@gmail.com>
2019-10-09 13:56:25 -0400

    created character arrays and ranodmization confirms

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+
+// Random character arrays //
+var specialOptions = [" ", "!", "#", "$", "%", "&", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=","?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Initial user input prompt, determining password length //
 var userInput = prompt("How many characters would you like your password to contain?");
 
 if (userInput < 8 || userInput > 128) {
     alert("Password must be between 8 and 128 characters");
+} 
+
+// Confirming use of each character type in password and generating random character result //
+var confirmSpecial = confirm("Click OK to confirm including special characters");
+    
+if (confirmSpecial) {
+    var randSpecial = specialOptions[Math.floor(Math.random() * specialOptions.length)];
+ }
+
+var confirmNumber = confirm("Click OK to confirm including numeric characters");
+
+if (confirmNumber) {
+    var randNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+}
+
+var confirmLower = confirm("Click OK to confirm including lowercase characters");
+
+if (confirmLower) {
+    var randLower = lowerOptions[Math.floor(Math.random() * lowerOptions.length)];
+}
+
+var confirmUpper = confirm("Click OK to confirm including uppercase characters");
+
+if (confirmUpper) {
+    var randUpper = upperOptions[Math.floor(Math.random() * upperOptions.length)];
 }
