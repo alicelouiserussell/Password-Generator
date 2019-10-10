0f355bebcef055d20a888563f8b6117b548af93d
Alice Russell <alice.louise.russell@gmail.com>
2019-10-10 16:18:29 -0400

    made generate button functional in creating random password

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Random character arrays //
 var specialOptions = ["!", "#", "$", "%", "&", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=","?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -7,59 +8,55 @@ var allOptions = [" ", "!", "#", "$", "%", "&", "()", "*", "+", ",", "-", ".", "
 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Initial user input prompt, determining password length //
-var userLength = prompt("How many characters would you like your password to contain?");
-
-if (userLength < 8 || userLength > 128) {
-    alert("Password must be between 8 and 128 characters");
-    prompt("How many characters would you like your password to contain?");
-}
-
-// Confirming use of each character type in password // 
-// Generating random character for each type //
-var confirmSpecial = confirm("Click OK to confirm including special characters");
+function generate() {
+    var userLength = prompt("How many characters would you like your password to contain?");
+    if (userLength < 8 || userLength > 128) {
+        alert("Password must be between 8 and 128 characters");
+        prompt("How many characters would you like your password to contain?");
+    }
+    var confirmSpecial = confirm("Click OK to confirm including special characters");
     
-if (confirmSpecial) {
+    if (confirmSpecial) {
     var randSpecial = specialOptions[Math.floor(Math.random() * specialOptions.length)];
- }
+    }
 
-var confirmNumber = confirm("Click OK to confirm including numeric characters");
+    var confirmNumber = confirm("Click OK to confirm including numeric characters");
 
-if (confirmNumber) {
+    if (confirmNumber) {
     var randNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
-}
+    }
 
-var confirmLower = confirm("Click OK to confirm including lowercase characters");
+    var confirmLower = confirm("Click OK to confirm including lowercase characters");
 
-if (confirmLower) {
+    if (confirmLower) {
     var randLower = lowerOptions[Math.floor(Math.random() * lowerOptions.length)];
-}
+    }
 
-var confirmUpper = confirm("Click OK to confirm including uppercase characters");
+    var confirmUpper = confirm("Click OK to confirm including uppercase characters");
 
-if (confirmUpper) {
+    if (confirmUpper) {
     var randUpper = upperOptions[Math.floor(Math.random() * upperOptions.length)];
+    }
+
+    if ((confirmSpecial === true) && (confirmNumber === true) && (confirmLower === true) && (confirmUpper === true)){
+        console.log(randSpecial + randNumber + randLower + randUpper);
+        }
+    
+    var passPartOne = randSpecial + randNumber + randLower + randUpper;
+    var passRemain = userLength - 4;
+    console.log(passRemain);
+    
+    var passPartTwo = "";
+    for (var i = 0; i <= passRemain; i++){
+       passPartTwo += allOptions[Math.floor(Math.random() * allOptions.length)];
+    }
+    
+    var randomPassword = passPartOne + passPartTwo;
+    console.log(randomPassword);
+    
+    var password = document.getElementById("password");
+    
+    password.textContent = randomPassword;
+    password.appendChild(randomPassword);
 }
-
-
-if ((confirmSpecial === true) && (confirmNumber === true) && (confirmLower === true) && (confirmUpper === true)){
-    console.log(randSpecial + randNumber + randLower + randUpper);
-}
-
-var passPartOne = randSpecial + randNumber + randLower + randUpper;
-var passRemain = userLength - 4;
-console.log(passRemain);
-
-var passPartTwo = "";
-for (var i = 0; i <= passRemain; i++){
-   passPartTwo += allOptions[Math.floor(Math.random() * allOptions.length)];
-}
-
-var randomPassword = passPartOne + passPartTwo;
-console.log(randomPassword);
-
-var password = document.getElementById("password");
-
-password.textContent = randomPassword;
-password.appendChild(randomPassword);
 
